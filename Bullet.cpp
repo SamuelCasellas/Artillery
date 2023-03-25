7b304dd1136407b9ccbeabd3da066431aae9e48a
Samuel Casellas <sam.casellas@gmail.com>
2023-03-24 22:14:22 -0600

    still not working...

--- Bullet.cpp
+++ Bullet.cpp
@@ -12,17 +12,17 @@
 Bullet::Bullet() : surfaceArea(0.0), mass(0.0), age(0.0) {}
 
 Bullet::Bullet(double mass, double radius, double initVel, Angle a) : mass(mass), surfaceArea(M_PI * radius * radius) {
-    
+
     // How long the
     this->age = 0.0;
-    
+
     this->ptBullet.setMetersX(0.0);
     this->ptBullet.setMetersY(0.0);
-    
+
     this->totalVelocity = this->totalAcceleration = initVel;
-    
+
     this->aBullet = a;
-    
+
     // Environmental factors at ground level.
                                                                 // if y = 0...
     this->density = Physics::interpolateDensity(0.0);           // 1.225
@@ -37,6 +37,8 @@ Bullet::Bullet(double mass, double radius, double initVel, Angle a, Position pos
     this->ptBullet = pos;
     
     this->totalVelocity = this->totalAcceleration = initVel;
+    
+    this->dx = this->dy = this->ddx = this->ddy = 0.0;
     
     this->aBullet = a;
     this->aDrag = Angle(a);
@@ -71,117 +73,79 @@ bool Bullet::hasLanded() {
 }
 
 void Bullet::calculateNewPosition() {
-    
     // t was half a second according to simulation
     const double t = 1.0;
-    
     // initial projectile force
     this->mach = Physics::calculateMach(this->totalVelocity, this->speedOfSound);
     this->dragCoefficient = Physics::interpolateDragCoefficient(this->mach);
-    
-//    this->projectileForce = Physics::calculateForce(this->mass, this->totalVelocity); // The velocity is the acceleration at the first frame
-
     double finalDistanceFromOrigin = 0.0;
     double hangTime = 0.0;
-
-    while (this->ptBullet.getMetersY() >= 0) {
-        // Collect all data from tables
-        // 2. Update to mach required for drag coefficient
+    
+    // double dragDdy = ;
+    
+    /** TODO: Make sure that we use horizontal, vertical, and total formulas
+         
+      
         
+        - Apply the necessary data to your  drag coefficient equation**
+            - This drag force is the total acceleration being applied to the projectile**
+        -  Calculate the angle of the projectile using the angle from components equation**
+        -  Calculate the inverse of the projectile (the inverse of 90 is 270)**
+        -  Apply the inverse angle of your drag force to get its vertical and horizontal components (sin and cos equations)**
+        -  Apply the sin and cos to your projectile's velocity
+        -  Apply your horizontal and vertical drag accelerations to your horizontal and vertical velocities
+        -  Apply gravity to your vertical velocity
+        -  Apply your velocities to your x/y position to update your position
+        -  Recombine your velocities into your total velocity using the pythagorean theorem
+        -  Output your data
+    */
+    
+    // Make a while loop to loop while your y value is over 0
+    while (this->ptBullet.getMetersY() >= 0) {
+        
+        /// - Collect all of the data from your tables using a table reading method and your linear interpolation methods**
         double y = this->ptBullet.getMetersY();
         this->density = Physics::interpolateDensity(y);
         this->speedOfSound = Physics::interpolateSpeedOfSound(y);
         this->gravity = Physics::interpolateGravity(y);
-
-        // 3. Apply necessary data to drag coefficient equation
-        // TOTAL ACCELERATION
         
+        // This drag force is the total acceleration being applied to the projectile
         this->dragForce = Physics::calculateDragForce(this->dragCoefficient, this->density, this->totalVelocity, this->surfaceArea);
-
-        // 4. Compute new angle (optional)
         
-        // Horizontal and verticle of drag force
-        double dragDx = Physics::calculateHorizontalComponent(this->aDrag.getRadians(), -(this->dx));
-        double dragDy = Physics::calculateVerticalComponent(this->aDrag.getRadians(), -(this->dy));
+        // Calculate horizontal and vertical components of the drag force
+        double dragForceDx = Physics::calculateHorizontalComponent(this->aDrag.getRadians(), this->dragForce);
+        double dragForceDy = Physics::calculateVerticalComponent(this->aDrag.getRadians(), this->dragForce);
         
-        /** TODO: Make sure that we use horizontal, vertical, and total formulas
-             
-          -Make a while loop to loop while your y value is over 0 that:
-            - Collect all of the data from your tables using a table reading method and your linear interpolation methods
-            - Apply the necessary data to your  drag coefficient equation
-                - This drag force is the total acceleration being applied to the projectile
-            -  Calculate the angle of the projectile using the angle from components equation
-            -  Calculate the inverse of the projectile (the inverse of 90 is 270)
-            -  Apply the inverse angle of your drag force to get its vertical and horizontal components (sin and cos equations)
-            -  Apply the sin and cos to your projectile's velocity
-            -  Apply your horizontal and vertical drag accelerations to your horizontal and vertical velocities
-            -  Apply gravity to your vertical velocity
-            -  Apply your velocities to your x/y position to update your position
-            -  Recombine your velocities into your total velocity using the pythagorean theorem
-            -  Output your data
-        */
-        
-        
-        // Physics::calculateT
-        
-        
-        // ?? Do we need this ??
-//        this->totalAcceleration = Physics::calculateAcceleration(this->dragForce /*FIX*/, this->mass);
-
-        // Velocity horizontal and vertical components
-        // double revAngle = this->reverseRadianAngle(this->aRadians);
         double dx0 = Physics::calculateHorizontalComponent(this->aBullet.getRadians(), this->totalVelocity);
         double dy0 = Physics::calculateVerticalComponent(this->aBullet.getRadians(), this->totalVelocity);
-
-        // Acceleration horizontal and vertical components
-        this->ddx = -(Physics::calculateHorizontalComponent(this->aBullet.getRadians(), this->totalAcceleration));
-        this->ddy = -(this->gravity) - Physics::calculateVerticalComponent(this->aBullet.getRadians(), this->totalAcceleration);
-
-        // New position
-
+       
+        // Add drag force components to the acceleration components (divided by the mass of the bullet)
+        this->ddx = -(Physics::calculateHorizontalComponent(this->aBullet.getRadians(), this->totalAcceleration)) - (dragForceDx / this->mass);
+        this->ddy = -(this->gravity) - Physics::calculateVerticalComponent(this->aBullet.getRadians(), this->totalAcceleration) - (dragForceDy / this->mass);
+        
+        this->totalAcceleration = Physics::calculateTotalVelocity(this->ddx, this->ddy);
+        
         double oldX = this->ptBullet.getMetersX();
         double oldY = this->ptBullet.getMetersY();
-
         this->ptBullet.setMetersX(Physics::calculateDistance(oldX, dx0, this->ddx, t));
         this->ptBullet.setMetersY(Physics::calculateDistance(oldY, dy0, this->ddy, t));
-
-        // Check if it hit the ground
-        // If so, determine where exactly
         if (this->ptBullet.getMetersY() <= 0.0) {
             finalDistanceFromOrigin = Physics::linearInterpolation(oldX, this->ptBullet.getMetersX(), oldY, this->ptBullet.getMetersY(), 0);
             hangTime = Physics::linearInterpolation(t, t+1, oldY, this->ptBullet.getMetersY(), 0);
             break;
         }
-
-        // New horizontal and vertical components
         this->dx = Physics::kinematicsEquation(dx0, this->ddx, t);
         this->dy = Physics::kinematicsEquation(dy0, this->ddy, t);
-
-        // Compute new angle (Wasn't in vid)
-        this->aBullet.setRadians(Physics::calculateAngleFromComponents(this->dx, this->dy));
         
+        this->aBullet.setRadians(Physics::calculateAngleFromComponents(this->dx, this->dy));
         this->aDrag = Angle(this->aBullet);
         this->aDrag.reverseRadianAngle();
-
-        // New velocity
-        this->totalVelocity = Physics::calculateTotalVelocity(this->dx, this->dy); // don't use initial?
-
-        // Include this??? (Wasn't in vid)
-        this->projectileForce = Physics::calculateForce(this->mass, this->totalAcceleration);
-        // cout << projectileForce << ' ' << dForce << '\n';
-
-        // s = s1 + (a * t) // where s is speed, s1 is initial speed, a is acceleration and t is time.
-        // Compute the new velocity
-//        dx = this->pythagorean(dx, ddx); // This is the wrong equation
-//        dy = this->pythagorean(dy, ddy);
-//
-        // cout << position.getMetersY() << endl;
-
+        this->totalVelocity = Physics::calculateTotalVelocity(this->dx, this->dy);
+        // this->projectileForce = Physics::calculateForce(this->mass, this->totalAcceleration);
         this->age += t;
-        
         this->mach = Physics::calculateMach(this->totalVelocity, this->speedOfSound);
         this->dragCoefficient = Physics::interpolateDragCoefficient(this->mach);
-   }
+    }
 }
 
 double Bullet::getAge()const{
